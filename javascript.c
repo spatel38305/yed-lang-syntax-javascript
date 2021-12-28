f4f9468e4c40723778e83893150baead45ce671f
spatel38305 <spatel84@vols.utk.edu>
2021-12-27 23:18:45 -0600

    Updated syntax highlighter to use new syntax code.

--- javascript.c
+++ javascript.c
@@ -1,184 +1,321 @@
 #include <yed/plugin.h>
-#include <yed/highlight.h>
+#include <yed/syntax.h>
 
-#define ARRAY_LOOP(a) for (__typeof((a)[0]) *it = (a); it < (a) + (sizeof(a) / sizeof((a)[0])); ++it)
+static yed_syntax syn;
 
-highlight_info hinfo;
+#define _CHECK(x, r)                                                      \
+do {                                                                      \
+    if (x) {                                                              \
+        LOG_FN_ENTER();                                                   \
+        yed_log("[!] " __FILE__ ":%d regex error for '%s': %s", __LINE__, \
+                r,                                                        \
+                yed_syntax_get_regex_err(&syn));                          \
+        LOG_EXIT();                                                       \
+    }                                                                     \
+} while (0)
 
-void unload(yed_plugin *self);
-void syntax_js_line_handler(yed_event *event);
-void syntax_js_frame_handler(yed_event *event);
-void syntax_js_buff_mod_pre_handler(yed_event *event);
-void syntax_js_buff_mod_post_handler(yed_event *event);
+#define SYN()          yed_syntax_start(&syn)
+#define ENDSYN()       yed_syntax_end(&syn)
+#define APUSH(s)       yed_syntax_attr_push(&syn, s)
+#define APOP(s)        yed_syntax_attr_pop(&syn)
+#define RANGE(r)       _CHECK(yed_syntax_range_start(&syn, r), r)
+#define ONELINE()      yed_syntax_range_one_line(&syn)
+#define SKIP(r)        _CHECK(yed_syntax_range_skip(&syn, r), r)
+#define ENDRANGE(r)    _CHECK(yed_syntax_range_end(&syn, r), r)
+#define REGEX(r)       _CHECK(yed_syntax_regex(&syn, r), r)
+#define REGEXSUB(r, g) _CHECK(yed_syntax_regex_sub(&syn, r, g), r)
+#define KWD(k)         yed_syntax_kwd(&syn, k)
 
+#ifdef __APPLE__
+#define WB "[[:>:]]"
+#else
+#define WB "\\b"
+#endif
+
+void estyle(yed_event *event)   { yed_syntax_style_event(&syn, event);         }
+void ebuffdel(yed_event *event) { yed_syntax_buffer_delete_event(&syn, event); }
+void ebuffmod(yed_event *event) { yed_syntax_buffer_mod_event(&syn, event);    }
+void eline(yed_event *event)  {
+    yed_frame *frame;
+
+    frame = event->frame;
+
+    if (!frame
+    ||  !frame->buffer
+    ||  frame->buffer->kind != BUFF_KIND_FILE
+    ||  frame->buffer->ft != yed_get_ft("JavaScript")) {
+        return;
+    }
+
+    yed_syntax_line_event(&syn, event);
+}
+
+void unload(yed_plugin *self) {
+    yed_syntax_free(&syn);
+    ys->redraw = 1;
+}
 
 int yed_plugin_boot(yed_plugin *self) {
-    yed_event_handler frame, line, buff_mod_pre, buff_mod_post;
-    char              *kwds[] = {
-        "abstract", "arguments", "await",
-        "class", "const",
-        "debugger", "delete",
-        "enum", "eval", "export", "extends",
-        "final", "finally", "function",
-        "implements", "import", "in", "instanceof", "interface",
-        "let",
-        "native", "new",
-        "package", "private", "protected", "public",
-        "static", "super", "synchronized",
-        "transient", "typeof",
-        "var", "volatile",
-    };
-    char              *control_flow[] = {
-        "break",
-        "case", "catch", "continue",
-        "default", "do",
-        "else",
-        "for",
-        "goto",
-        "if",
-        "return",
-        "switch",
-        "throw", "throws", "try",
-        "while",
-        "yield",
-    };
-    char              *typenames[] = {
-        "boolean", "byte",
-        "char",
-        "double",
-        "float",
-        "long",
-        "int",
-        "short",
-        "unsigned",
-        "void",
-    };
-    char              *other[] = {
-        "Array",    "Date",
-        "hasOwnProperty",    "Infinity",    "isFinite",    "isNaN",
-        "isPrototypeOf",    "length",    "Math",    "NaN",
-        "name",    "Number",    "Object",    "prototype",
-        "String",    "toString",    "undefined",    "valueOf",
-        "onblur",    "onclick",    "onerror",    "onfocus",
-        "onkeydown",    "onkeypress",    "onkeyup",    "onmouseover",
-        "onload",    "onmouseup",    "onmousedown",    "onsubmit",
-        "alert",    "all",    "anchor",    "anchors",
-        "area",    "assign",    "blur",    "button",
-        "checkbox",    "clearInterval",    "clearTimeout",    "clientInformation",
-        "close",    "closed",    "confirm",    "constructor",
-        "crypto",    "decodeURI",    "decodeURIComponent",    "defaultStatus",
-        "document",    "element",    "elements",    "embed",
-        "embeds",    "encodeURI",    "encodeURIComponent",    "escape",
-        "event",    "fileUpload",    "focus",    "form",
-        "forms",    "frame",    "innerHeight",    "innerWidth",
-        "layer",    "layers",    "link",    "location",
-        "mimeTypes",    "navigate",    "navigator",    "frames",
-        "frameRate",    "hidden",    "history",    "image",
-        "images",    "offscreenBuffering",    "open",    "opener",
-        "option",    "outerHeight",    "outerWidth",    "packages",
-        "pageXOffset",    "pageYOffset",    "parent",    "parseFloat",
-        "parseInt",    "password",    "pkcs11",    "plugin",
-        "prompt",    "propertyIsEnum",    "radio",    "reset",
-        "screenX",    "screenY",    "scroll",    "secure",
-        "select",    "self",    "setInterval",    "setTimeout",
-        "status",    "submit",    "taint",    "text",
-        "textarea",    "top",    "unescape",    "untaint",
-        "window", "true", "false", "this", "null",
-    };
+    yed_event_handler style;
+    yed_event_handler buffdel;
+    yed_event_handler buffmod;
+    yed_event_handler line;
 
     YED_PLUG_VERSION_CHECK();
 
     yed_plugin_set_unload_fn(self, unload);
 
+    style.kind = EVENT_STYLE_CHANGE;
+    style.fn   = estyle;
+    yed_plugin_add_event_handler(self, style);
 
-    frame.kind          = EVENT_FRAME_PRE_BUFF_DRAW;
-    frame.fn            = syntax_js_frame_handler;
-    line.kind           = EVENT_LINE_PRE_DRAW;
-    line.fn             = syntax_js_line_handler;
-    buff_mod_pre.kind   = EVENT_BUFFER_PRE_MOD;
-    buff_mod_pre.fn     = syntax_js_buff_mod_pre_handler;
-    buff_mod_post.kind  = EVENT_BUFFER_POST_MOD;
-    buff_mod_post.fn    = syntax_js_buff_mod_post_handler;
+    buffdel.kind = EVENT_BUFFER_PRE_DELETE;
+    buffdel.fn   = ebuffdel;
+    yed_plugin_add_event_handler(self, buffdel);
 
-    yed_plugin_add_event_handler(self, frame);
+    buffmod.kind = EVENT_BUFFER_POST_MOD;
+    buffmod.fn   = ebuffmod;
+    yed_plugin_add_event_handler(self, buffmod);
+
+    line.kind = EVENT_LINE_PRE_DRAW;
+    line.fn   = eline;
+
     yed_plugin_add_event_handler(self, line);
-    yed_plugin_add_event_handler(self, buff_mod_pre);
-    yed_plugin_add_event_handler(self, buff_mod_post);
 
+    SYN();
+        APUSH("&code-comment");
+            RANGE("/\\*");
+            ENDRANGE(  "\\*/");
+            RANGE("//");
+                ONELINE();
+            ENDRANGE("$");
+            RANGE("^[[:space:]]*#[[:space:]]*if[[:space:]]+0"WB);
+            ENDRANGE("^[[:space:]]*#[[:space:]]*(else|endif|elif|elifdef)"WB);
+        APOP();
 
-    highlight_info_make(&hinfo);
+        APUSH("&code-string");
+            REGEX("'(\\\\.|[^'\\\\])'");
 
-    ARRAY_LOOP(kwds)
-        highlight_add_kwd(&hinfo, *it, HL_KEY);
-    ARRAY_LOOP(control_flow)
-        highlight_add_kwd(&hinfo, *it, HL_CF);
-    ARRAY_LOOP(typenames)
-        highlight_add_kwd(&hinfo, *it, HL_TYPE);
-    ARRAY_LOOP(other)
-        highlight_add_kwd(&hinfo, *it, HL_CON);
-    highlight_suffixed_words(&hinfo, '(', HL_CALL);
-    highlight_numbers(&hinfo);
-    highlight_within(&hinfo, "\"", "\"", '\\', -1, HL_STR);
-    highlight_within(&hinfo, "'", "'", '\\', -1, HL_STR);
-    highlight_to_eol_from(&hinfo, "//", HL_COMMENT);
-    highlight_within_multiline(&hinfo, "/*", "*/", 0, HL_COMMENT);
+            RANGE("\""); ONELINE(); SKIP("\\\\\"");
+                APUSH("&code-escape");
+                    REGEX("\\\\.");
+                APOP();
+            ENDRANGE("\"");
+
+            RANGE("\'"); ONELINE(); SKIP("\\\\\"");
+                APUSH("&code-escape");
+                    REGEX("\\\\.");
+                APOP();
+            ENDRANGE("\'");
+        APOP();
+
+        APUSH("&code-fn-call");
+            REGEXSUB("([[:alpha:]_][[:alnum:]_]*)[[:space:]]*\\(", 1);
+        APOP();
+
+        APUSH("&code-number");
+            REGEXSUB("(^|[^[:alnum:]_])(-?([[:digit:]]+\\.[[:digit:]]*)|(([[:digit:]]*\\.[[:digit:]]+))(e\\+[[:digit:]]+)?[fFlL]?)"WB, 2);
+            REGEXSUB("(^|[^[:alnum:]_])(-?[[:digit:]]+(([uU]?[lL]{0,2})|([lL]{0,2}[uU]?))?)"WB, 2);
+            REGEXSUB("(^|[^[:alnum:]_])(0[xX][0-9a-fA-F]+(([uU]?[lL]{0,2})|([lL]{0,2}[uU]?))?)"WB, 2);
+        APOP();
+
+        APUSH("&code-keyword");
+            KWD("abstract");
+            KWD("arguments");
+            KWD("await");
+            KWD("class");
+            KWD("const");
+            KWD("debugger");
+            KWD("delete");
+            KWD("enum");
+            KWD("eval");
+            KWD("export");
+            KWD("extends");
+            KWD("final");
+            KWD("finally");
+            KWD("function");
+            KWD("implements");
+            KWD("import");
+            KWD("in");
+            KWD("instanceof");
+            KWD("interface");
+            KWD("let");
+            KWD("native");
+            KWD("new");
+            KWD("package");
+            KWD("private");
+            KWD("protected");
+            KWD("public");
+            KWD("static");
+            KWD("super");
+            KWD("synchronized");
+            KWD("transient");
+            KWD("typeof");
+            KWD("var");
+            KWD("volatile");
+        APOP();
+
+        APUSH("&code-control-flow");
+            KWD("break");
+            KWD("case");
+            KWD("catch");
+            KWD("continue");
+            KWD("default");
+            KWD("do");
+            KWD("else");
+            KWD("for");
+            KWD("goto");
+            KWD("if");
+            KWD("return");
+            KWD("switch");
+            KWD("throw");
+            KWD("throws");
+            KWD("try");
+            KWD("while");
+            KWD("yield");
+        APOP();
+
+        APUSH("&code-typename");
+            KWD("boolean");
+            KWD("byte");
+            KWD("char");
+            KWD("double");
+            KWD("float");
+            KWD("long");
+            KWD("int");
+            KWD("short");
+            KWD("unsigned");
+            KWD("void");
+        APOP();
+
+        APUSH("&code-constant");
+            KWD("Array");
+            KWD("Date");
+            KWD("hasOwnProperty");
+            KWD("Infinity");
+            KWD("isFinite");
+            KWD("isNaN");
+            KWD("isPrototypeOf");
+            KWD("length");
+            KWD("Math");
+            KWD("NaN");
+            KWD("name");
+            KWD("Number");
+            KWD("Object");
+            KWD("prototype");
+            KWD("String");
+            KWD("toString");
+            KWD("undefined");
+            KWD("valueOf");
+            KWD("onblur");
+            KWD("onclick");
+            KWD("onerror");
+            KWD("onfocus");
+            KWD("onkeydown");
+            KWD("onkeypress");
+            KWD("onkeyup");
+            KWD("onmouseover");
+            KWD("onload");
+            KWD("onmouseup");
+            KWD("onmousedown");
+            KWD("onsubmit");
+            KWD("alert");
+            KWD("all");
+            KWD("anchor");
+            KWD("anchors");
+            KWD("area");
+            KWD("assign");
+            KWD("blur");
+            KWD("button");
+            KWD("checkbox");
+            KWD("clearInterval");
+            KWD("clearTimeout");
+            KWD("clientInformation");
+            KWD("close");
+            KWD("closed");
+            KWD("confirm");
+            KWD("constructor");
+            KWD("crypto");
+            KWD("decodeURI");
+            KWD("decodeURIComponent");
+            KWD("defaultStatus");
+            KWD("document");
+            KWD("element");
+            KWD("elements");
+            KWD("embed");
+            KWD("embeds");
+            KWD("encodeURI");
+            KWD("encodeURIComponent");
+            KWD("escape");
+            KWD("event");
+            KWD("fileUpload");
+            KWD("focus");
+            KWD("form");
+            KWD("forms");
+            KWD("frame");
+            KWD("innerHeight");
+            KWD("innerWidth");
+            KWD("layer");
+            KWD("layers");
+            KWD("link");
+            KWD("location");
+            KWD("mimeTypes");
+            KWD("navigate");
+            KWD("navigator");
+            KWD("frames");
+            KWD("frameRate");
+            KWD("hidden");
+            KWD("history");
+            KWD("image");
+            KWD("images");
+            KWD("offscreenBuffering");
+            KWD("open");
+            KWD("opener");
+            KWD("option");
+            KWD("outerHeight");
+            KWD("outerWidth");
+            KWD("packages");
+            KWD("pageXOffset");
+            KWD("pageYOffset");
+            KWD("parent");
+            KWD("parseFloat");
+            KWD("parseInt");
+            KWD("password");
+            KWD("pkcs11");
+            KWD("plugin");
+            KWD("prompt");
+            KWD("propertyIsEnum");
+            KWD("radio");
+            KWD("reset");
+            KWD("screenX");
+            KWD("screenY");
+            KWD("scroll");
+            KWD("secure");
+            KWD("select");
+            KWD("self");
+            KWD("setInterval");
+            KWD("setTimeout");
+            KWD("status");
+            KWD("submit");
+            KWD("taint");
+            KWD("text");
+            KWD("textarea");
+            KWD("top");
+            KWD("unescape");
+            KWD("untaint");
+            KWD("window");
+            KWD("true");
+            KWD("false");
+            KWD("this");
+            KWD("null");
+        APOP();
+
+        APUSH("&code-field");
+            REGEXSUB("(\\.|->)[[:space:]]*([[:alpha:]_][[:alnum:]_]*)", 2);
+        APOP();
+    ENDSYN();
 
     ys->redraw = 1;
 
     return 0;
-}
-
-void unload(yed_plugin *self) {
-    highlight_info_free(&hinfo);
-    ys->redraw = 1;
-}
-
-void syntax_js_frame_handler(yed_event *event) {
-    yed_frame *frame;
-
-    frame = event->frame;
-
-    if (!frame
-    ||  !frame->buffer
-    ||  frame->buffer->kind != BUFF_KIND_FILE
-    ||  frame->buffer->ft != yed_get_ft("JavaScript")) {
-        return;
-    }
-
-    highlight_frame_pre_draw_update(&hinfo, event);
-}
-
-void syntax_js_line_handler(yed_event *event) {
-    yed_frame *frame;
-
-    frame = event->frame;
-
-    if (!frame
-    ||  !frame->buffer
-    ||  frame->buffer->kind != BUFF_KIND_FILE
-    ||  frame->buffer->ft != yed_get_ft("JavaScript")) {
-        return;
-    }
-
-    highlight_line(&hinfo, event);
-}
-
-void syntax_js_buff_mod_pre_handler(yed_event *event) {
-    if (event->buffer == NULL
-    ||  event->buffer->kind != BUFF_KIND_FILE
-    ||  event->buffer->ft != yed_get_ft("JavaScript")) {
-        return;
-    }
-
-    highlight_buffer_pre_mod_update(&hinfo, event);
-}
-
-void syntax_js_buff_mod_post_handler(yed_event *event) {
-    if (event->buffer == NULL
-    ||  event->buffer->kind != BUFF_KIND_FILE
-    ||  event->buffer->ft != yed_get_ft("JavaScript")) {
-        return;
-    }
-
-    highlight_buffer_post_mod_update(&hinfo, event);
 }
